858d1f656b6646a7a081c635b9e0613557b922a7
DESKTOP-IAF0JQK\wasif <wasifmunir86@gmail.com>
2024-11-11 13:13:14 +0500

    added code

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+/*************  ✨ Codeium Command ⭐  *************/
+  /**
+   * Returns a Hello World message
+   *
+   * @returns A hello world message
+   */
+/******  a7a517c2-0951-4062-9259-f11e7723566d  *******/
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! aye hoye';
   }
 }
